ae7e8321ea7a8d48da58789bed7afbab77174c34
Andrew Shell <andrew@andrewshell.org>
2017-09-10 15:12:15 +0000

    Disabling ParseLaravelToken

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,7 +38,6 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            \App\Http\Middleware\ParseLaravelToken::class,
             'throttle:60,1',
             'bindings',
         ],
