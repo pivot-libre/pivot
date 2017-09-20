e26f10ad8554a3001b6d2c9e5e128ca5d4161b6e
tylerharter <tylerharter@gmail.com>
2017-09-19 19:57:26 -0500

    Return Tideman computation using dummy values for GET to homestead.app/api/election/1/result

--- routes/api.php
+++ routes/api.php
@@ -22,5 +22,6 @@ Route::resource('election.elector', 'ElectorController', [
     'except' => ['create', 'store', 'edit', 'update'],
     'parameters' => ['elector' => 'user'],
 ]);
+Route::resource('election.result', 'ResultController', ['only' => ['index']]);
 Route::resource('election.invite', 'InviteController', ['only' => ['index', 'show', 'store', 'destroy']]);
 Route::post('/invite/accept', 'InviteController@accept')->name('invite.accept');
