8209b5b90fa30d4cdceb0817844087345c50bc38
tylerharter <tylerharter@gmail.com>
2018-02-10 10:20:01 -0600

    make test api an object

--- tests/python/tests.py
+++ tests/python/tests.py
@@ -20,146 +20,151 @@ class LatencyStats:
         for k in keys:
             print str(int(avg[k]*1000)).rjust(4) + ' ms   ' + k + ' [%d calls]' % len(self.latencies[k])
 
-LATENCY_STATS = LatencyStats()
-        
-def load_users():
-    with open('users.json') as f:
-        return json.loads(f.read())["users"]
+class API:
+    def __init__(self):
+        self.latency_stats = LatencyStats()
+    
+    def load_users(self):
+        with open('users.json') as f:
+            return json.loads(f.read())["users"]
 
-def dump(out):
-    with open('dump.html', 'w') as f:
-        f.write(out)
+    def dump(self, out):
+        with open('dump.html', 'w') as f:
+            f.write(out)
 
-# generic API
-def user_get(user, url):
-    global URL
-    print 'GET '+url
-    headers = {'Authorization': 'Bearer '+user['token']}
-    t0 = time.time()
-    r = requests.get(url = URL + '/' + url, headers=headers)
-    d = r.content
-    t1 = time.time()
-    LATENCY_STATS.add('GET '+url, t1-t0)
-    try:
-        return json.loads(d)
-    except:
-        print 'could not parse: ' + d[:100] + '...'
-        dump(d)
-        assert(0)
+    def dump_stats(self):
+        self.latency_stats.dump()
 
-def user_post(user, url, body):
-    global URL
-    print 'POST '+url
-    headers = {'Authorization': 'Bearer '+user['token']}
-    t0 = time.time()
-    r = requests.post(url = URL + '/' + url, headers=headers, data=json.dumps(body))
-    d = r.content
-    t1 = time.time()
-    LATENCY_STATS.add('POST '+url, t1-t0)
-    try:
-        return json.loads(d)
-    except:
-        print 'could not parse: ' + d[:100] + '...'
-        dump(d)
-        assert(0)
+    # generic API
+    def user_get(self, user, url):
+        global URL
+        print 'GET '+url
+        headers = {'Authorization': 'Bearer '+user['token']}
+        t0 = time.time()
+        r = requests.get(url = URL + '/' + url, headers=headers)
+        d = r.content
+        t1 = time.time()
+        self.latency_stats.add('GET '+url, t1-t0)
+        try:
+            return json.loads(d)
+        except:
+            print 'could not parse: ' + d[:100] + '...'
+            dump(d)
+            assert(0)
 
-# pivot API wrappers
-def get_elections(user):
-    return user_get(user, 'election')
+    def user_post(self, user, url, body):
+        global URL
+        print 'POST '+url
+        headers = {'Authorization': 'Bearer '+user['token']}
+        t0 = time.time()
+        r = requests.post(url = URL + '/' + url, headers=headers, data=json.dumps(body))
+        d = r.content
+        t1 = time.time()
+        self.latency_stats.add('POST '+url, t1-t0)
+        try:
+            return json.loads(d)
+        except:
+            print 'could not parse: ' + d[:100] + '...'
+            dump(d)
+            assert(0)
 
-def create_election(user, name):
-    return user_post(user, 'election', {"name": name})
+    # pivot API wrappers
+    def get_elections(self, user):
+        return self.user_get(user, 'election')
 
-def get_candidates(user, election):
-    url = 'election/%d/candidate' % election['id']
-    return user_get(user, url)
+    def create_election(self, user, name):
+        return self.user_post(user, 'election', {"name": name})
 
-def get_electors(user, election):
-    url = 'election/%d/elector' % election['id']
-    return user_get(user, url)
+    def get_candidates(self, user, election):
+        url = 'election/%d/candidate' % election['id']
+        return self.user_get(user, url)
 
-def create_candidate(user, election, name):
-    url = 'election/%d/candidate' % election['id']
-    return user_post(user, url, {"name": name})
+    def get_electors(self, user, election):
+        url = 'election/%d/elector' % election['id']
+        return self.user_get(user, url)
 
-def set_rank(user, election, candidate, rank):
-    url = 'election/%d/candidate/%d/rank' % (election['id'], candidate['id'])
-    return user_post(user, url, {"rank": rank})
+    def create_candidate(self, user, election, name):
+        url = 'election/%d/candidate' % election['id']
+        return self.user_post(user, url, {"name": name})
 
-def invite(user, election, email):
-    url = 'election/%d/invite' % (election['id'])
-    return user_post(user, url, {"email": email})
+    def set_rank(self, user, election, candidate, rank):
+        url = 'election/%d/candidate/%d/rank' % (election['id'], candidate['id'])
+        return self.user_post(user, url, {"rank": rank})
 
-def accept(user, code):
-    url = 'invite/accept'
-    return user_post(user, url, {"code": code})
+    def invite(self, user, election, email):
+        url = 'election/%d/invite' % (election['id'])
+        return self.user_post(user, url, {"email": email})
 
-def acceptable(user):
-    url = 'invite/acceptable'
-    return user_post(user, url, {})
+    def accept(self, user, code):
+        url = 'invite/accept'
+        return self.user_post(user, url, {"code": code})
 
-def election_result(user, election):
-    url = 'election/%d/result' % election['id']
-    return user_get(user, url)
+    def acceptable(self, user):
+        url = 'invite/acceptable'
+        return self.user_post(user, url, {})
 
-def batchvote(user, election, votes):
-    url = 'election/%d/batchvote' % election['id']
-    return user_post(user, url, {'votes': votes})
+    def election_result(self, user, election):
+        url = 'election/%d/result' % election['id']
+        return self.user_get(user, url)
 
-def batchvote_view(user, election):
-    url = 'election/%d/batchvote' % election['id']
-    return user_get(user, url)
+    def batchvote(self, user, election, votes):
+        url = 'election/%d/batchvote' % election['id']
+        return self.user_post(user, url, {'votes': votes})
 
-def test1():
+    def batchvote_view(self, user, election):
+        url = 'election/%d/batchvote' % election['id']
+        return self.user_get(user, url)
+
+def test1(api):
     print "\n============= TEST 1 ============\n"
-    users = load_users()
+    users = api.load_users()
     userA = users[0]
     userB = users[1]
 
     # election (A's view)
-    len1A = len(get_elections(userA))
-    len1B = len(get_elections(userB))
-    election = create_election(userA, 'test-election')
-    len2A = len(get_elections(userA))
-    len2B = len(get_elections(userB))
+    len1A = len(api.get_elections(userA))
+    len1B = len(api.get_elections(userB))
+    election = api.create_election(userA, 'test-election')
+    len2A = len(api.get_elections(userA))
+    len2B = len(api.get_elections(userB))
     assert(len2A == len1A + 1)
     assert(len2B == len1B)
 
     # election (B's view)
-    len1 = len(get_elections(userA))
-    election = create_election(userA, 'test-election')
-    len2 = len(get_elections(userA))
+    len1 = len(api.get_elections(userA))
+    election = api.create_election(userA, 'test-election')
+    len2 = len(api.get_elections(userA))
     assert(len2 == len1 + 1)
 
     # electors
-    invite_status = invite(userA, election, userA['email'])
+    invite_status = api.invite(userA, election, userA['email'])
     print 'invite_status: %s' % str(invite_status)
     code = invite_status['code']
     print code
-    accept(userA, code)
-    invite_status = invite(userA, election, userB['email'])
+    api.accept(userA, code)
+    invite_status = api.invite(userA, election, userB['email'])
     print 'invite_status: %s' % str(invite_status)
     code = invite_status['code']
-    electors = get_electors(userA, election)
+    electors = api.get_electors(userA, election)
     assert(len(electors) == 1)
-    acceptables = acceptable(userB)
+    acceptables = api.acceptable(userB)
     codes = [inv['code'] for inv in acceptables]
     assert (code in codes)
-    accept(userB, code)
-    electors = get_electors(userA, election)
+    api.accept(userB, code)
+    electors = api.get_electors(userA, election)
     assert(len(electors) == 2)
-    electors = get_electors(userB, election)
+    electors = api.get_electors(userB, election)
     assert(len(electors) == 2)
 
     # verify B can now see it after being added
-    len3B = len(get_elections(userB))
+    len3B = len(api.get_elections(userB))
     assert(len3B == len2B + 1)
 
     # candidates
-    A = create_candidate(userA, election, 'candidate-A')
-    B = create_candidate(userA, election, 'candidate-B')
-    C = create_candidate(userA, election, 'candidate-C')
-    assert(len(get_candidates(userA, election)) == 3)
+    A = api.create_candidate(userA, election, 'candidate-A')
+    B = api.create_candidate(userA, election, 'candidate-B')
+    C = api.create_candidate(userA, election, 'candidate-C')
+    assert(len(api.get_candidates(userA, election)) == 3)
 
     # vote
     batch = True
@@ -168,33 +173,33 @@ def test1():
         {'candidate_id': B['id'], 'rank': 1},
         {'candidate_id': C['id'], 'rank': 3},
     ]
-    bv1 = batchvote(userA, election, votes)
-    bv2 = batchvote_view(userA, election)
+    bv1 = api.batchvote(userA, election, votes)
+    bv2 = api.batchvote_view(userA, election)
     assert(len(bv1)) == len(votes)
     assert(len(bv2)) == len(votes)
 
     # result
-    print election_result(userA, election)
-    print election_result(userB, election)
+    print api.election_result(userA, election)
+    print api.election_result(userB, election)
 
-def test2():
+def test2(api):
     """
     This tests one user voting on their own election.
     """
     print "\n============= TEST 2 ============\n"
-    users = load_users()
+    users = api.load_users()
     userA = users[0]
     userB = users[1]
 
-    election = create_election(userA, 'Triceritops Rex')
-    invite_status = invite(userA, election, userA['email'])
+    election = api.create_election(userA, 'Triceritops Rex')
+    invite_status = api.invite(userA, election, userA['email'])
     code = invite_status['code']
-    accept(userA, code)
+    api.accept(userA, code)
 
-    A = create_candidate(userA, election, 'candidate-A')
-    B = create_candidate(userA, election, 'candidate-B')
-    C = create_candidate(userA, election, 'candidate-C')
-    D = create_candidate(userA, election, 'candidate-D')
+    A = api.create_candidate(userA, election, 'candidate-A')
+    B = api.create_candidate(userA, election, 'candidate-B')
+    C = api.create_candidate(userA, election, 'candidate-C')
+    D = api.create_candidate(userA, election, 'candidate-D')
 
     votes = [
         {'candidate_id': A['id'], 'rank': 1},
@@ -202,8 +207,8 @@ def test2():
         {'candidate_id': C['id'], 'rank': 3},
         {'candidate_id': D['id'], 'rank': 4},
     ]
-    bv1 = batchvote(userA, election, votes)
-    results = election_result(userA, election)
+    bv1 = api.batchvote(userA, election, votes)
+    results = api.election_result(userA, election)
     result_names = [result['name'] for result in results['order']]
     assert(result_names == [u'candidate-A', u'candidate-B', u'candidate-C', u'candidate-D'])
     print result_names
@@ -214,11 +219,12 @@ def main(url = ''):
         URL = url
     else:
         print "\n no url specified. Using default " + URL
-    test1()
-    test2()
+    api = API()
+    test1(api)
+    test2(api)
 
     print "\n============= LATENCY STATUS ============\n"
-    LATENCY_STATS.dump()
+    api.dump_stats()
 
 if __name__ == '__main__':
     main(*sys.argv[1:])
