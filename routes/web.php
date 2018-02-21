2cd317c4450be16086501c8a74baa60b0a8e2643
Nathan Eckberg <nathan.eckberg@gmail.com>
2018-02-21 15:46:37 +0000

    redirect to login page when unauthenticated (issue #83)

--- routes/web.php
+++ routes/web.php
@@ -11,41 +11,49 @@
 |
 */
 
-Route::get('/', function () {
-  return view('myElections');
-});
-
 Auth::routes();
 
 Route::get('/profile', 'ProfileController@index');
 Route::get('/profile/accept', 'ProfileController@accept');
-Route::get('/ballot/{election}', function ($election) {
-    return view('ballot', ['election' => $election]);
+
+
+// all routes that require authentication go in here, the user will be redirected to the login page,
+// and will then be directed to their initial target once they log in
+Route::group(['middleware' => 'auth'], function() {
+  // Route::get('string', ['as' => 'user.add_event', 'uses' => 'UserController@addEvent']);
+  Route::get('/', function () {
+    return view('myElections');
+  });
+  Route::get('/myElections', function () {
+    return view('myElections');
+  });
+  Route::get('/ballot/{election}', function ($election) {
+      return view('ballot', ['election' => $election]);
+  });
+  Route::get('/create', function () {
+      return view('create');
+  });
+  Route::get('/administer/{election}', function ($election) {
+      return view('administer', ['election' => $election]);
+  });
+  Route::get('/candidates/{election}', function ($election) {
+      return view('candidates', ['election' => $election]);
+  });
+  Route::get('/electorate/{election}', function ($election) {
+      return view('electorate', ['election' => $election]);
+  });
+  Route::get('/results/{election}', function ($election) {
+      return view('results', ['election' => $election]);
+  });
 });
+
 Route::get('/ballotReview/{election}', function ($election) {
     return view('ballotReview', ['election' => $election]);
 });
-Route::get('/elections', function () {
-    return view('elections');
-});
-Route::get('/create', function () {
-    return view('create');
-});
-Route::get('/myElections', function () {
-  return view('myElections');
-});
-Route::get('/administer/{election}', function ($election) {
-    return view('administer', ['election' => $election]);
-});
-Route::get('/candidates/{election}', function ($election) {
-    return view('candidates', ['election' => $election]);
-});
-Route::get('/electorate/{election}', function ($election) {
-    return view('electorate', ['election' => $election]);
-});
-Route::get('/results/{election}', function ($election) {
-    return view('results', ['election' => $election]);
-});
+// // maybe /myElections should be renamed to /elections
+// Route::get('/elections', function () {
+//     return view('elections');
+// });
 Route::get('/verify_email', function () {
   return view('auth/verifyEmail');
 })->name('verify_email');
