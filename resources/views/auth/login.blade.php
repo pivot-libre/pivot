0537daccbfd6855a39cf53d22ff588265ea139f7
Nathan Eckberg <nathan.eckberg@gmail.com>
2018-02-21 04:10:26 +0000

    update styles for login and register pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,57 +2,53 @@
 
 @section('content')
 
+<form class="w75 textLeft" role="form" method="POST" action="{{ route('login') }}">
+    {{ csrf_field() }}
 
-                    <form class="w75 textLeft" role="form" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
+    <div class="row1{{ $errors->has('email') ? ' has-error' : '' }}">
+        <label for="email" class="w33">E-Mail Address</label>
 
-                        <div class="row1{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="w33">E-Mail Address</label>
+        <div class="w67">
+            <input id="email" class="w67" type="email" name="email" value="{{ old('email') }}" required autofocus>
+        </div>
+        @if ($errors->has('email'))
+          <div class="w33"></div>
+          <div class="text2"> {{ $errors->first('email') }} </div>
+        @endif
 
-                            <div class="w67">
-                                <input id="email" class="w67" type="email" name="email" value="{{ old('email') }}" required autofocus>
+    </div>
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+    <div class="row1{{ $errors->has('password') ? ' has-error' : '' }}">
+        <label for="password" class="w33">Password</label>
 
-                        <div class="row1{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="w33">Password</label>
+        <div class="w67">
+            <input id="password" class="w67" type="password" class="textinput1" name="password" required>
+        </div>
+        @if ($errors->has('password'))
+          <div class="w33"></div>
+          <div class="text2"> {{ $errors->first('password') }} </div>
+        @endif
+    </div>
 
-                            <div class="w67">
-                                <input id="password" class="w67" type="password" class="textinput1" name="password" required>
+    <div class="w100">
+        <div class="w33"></div>
+        <label>
+            <input type="checkbox" class="checkbox1" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+        </label>
+    </div>
 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+    <div class="row1">
+      <div class="w33"></div>
+      <div>
+          <button type="submit" class="">
+              Login
+          </button>
 
-                        <div class="w100">
-                            <div class="w33"></div>
-                            <label>
-                                <input type="checkbox" class="checkbox1" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                            </label>
-                        </div>
-
-                        <div class="row1">
-                          <div class="w33"></div>
-                          <div class="col-md-8 col-md-offset-4">
-                              <button type="submit" class="">
-                                  Login
-                              </button>
-
-                              <a class="a1" href="{{ route('password.request') }}">
-                                  Forgot Your Password?
-                              </a>
-                          </div>
-                        </div>
-                    </form>
+          <a class="a1" href="{{ route('password.request') }}">
+              Forgot Your Password?
+          </a>
+      </div>
+    </div>
+</form>
 
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,90 +1,77 @@
-@extends('layouts.app')
+@extends('layouts.pivot')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('token') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Verification Token</label>
+<form class="w75 textLeft" role="form" method="POST" action="{{ route('register') }}">
+    {{ csrf_field() }}
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="token" value="{{ $token }}" autofocus>
+    <div class="row1{{ $errors->has('token') ? ' has-error' : '' }}">
+        <label for="name" class="w33">Verification Token</label>
 
-                                @if ($errors->has('token'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('token') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+        <div class="w67">
+            <input id="name" type="text" class="w67" name="token" value="{{ $token }}" autofocus>
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+        </div>
+        @if ($errors->has('token'))
+          <div class="w33"></div>
+          <div class="text2"> {{ $errors->first('token') }} </div>
+        @endif
+    </div>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+    <div class="row1{{ $errors->has('name') ? ' has-error' : '' }}">
+        <label for="name" class="w33">Name</label>
 
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+        <div class="w67">
+            <input id="name" type="text" class="w67" name="name" value="{{ old('name') }}" required autofocus>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+        </div>
+        @if ($errors->has('name'))
+          <div class="w33"></div>
+          <div class="text2"> {{ $errors->first('name') }} </div>
+        @endif
+    </div>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ $email }}" required>
+    <div class="row1{{ $errors->has('email') ? ' has-error' : '' }}">
+        <label for="email" class="w33">E-Mail Address</label>
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+        <div class="w67">
+            <input id="email" type="email" class="w67" name="email" value="{{ $email }}" required>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+        </div>
+        @if ($errors->has('email'))
+          <div class="w33"></div>
+          <div class="text2"> {{ $errors->first('email') }} </div>
+        @endif
+    </div>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+    <div class="row1{{ $errors->has('password') ? ' has-error' : '' }}">
+        <label for="password" class="w33">Password</label>
 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+        <div class="w67">
+            <input id="password" type="password" class="w67" name="password" required>
+        </div>
+        @if ($errors->has('password'))
+          <div class="w33"></div>
+          <div class="text2"> {{ $errors->first('password') }} </div>
+        @endif
+    </div>
 
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+    <div class="row1">
+        <label for="password-confirm" class="w33">Confirm Password</label>
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+        <div class="w67">
+            <input id="password-confirm" type="password" class="w67" name="password_confirmation" required>
         </div>
     </div>
-</div>
+
+    <div class="row1">
+      <div class="w33"></div>
+      <div>
+          <button type="submit" class="btn btn-primary">
+              Register
+          </button>
+      </div>
+    </div>
+</form>
+
 @endsection
