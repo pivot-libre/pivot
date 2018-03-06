84af4d0bae821d5eb5ee1ba323a7fb04deee62b4
Nathan Eckberg <nathan.eckberg@gmail.com>
2018-03-06 21:24:36 +0000

    add email verify to registration page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,6 +5,19 @@
 <form class="w75 textLeft" role="form" method="POST" action="{{ route('register') }}">
     {{ csrf_field() }}
 
+    <div class="row1{{ $errors->has('email') ? ' has-error' : '' }}">
+        <label for="email" class="w33">E-Mail Address</label>
+
+        <div class="w67">
+            <input id="email" type="email" class="w67" name="email" value="{{ $email }}" required>
+
+        </div>
+        @if ($errors->has('email'))
+          <div class="w33"></div>
+          <div class="text2"> {{ $errors->first('email') }} </div>
+        @endif
+    </div>
+
     <div class="row1{{ $errors->has('token') ? ' has-error' : '' }}">
         <label for="name" class="w33">Verification Token</label>
 
@@ -17,6 +30,29 @@
           <div class="text2"> {{ $errors->first('token') }} </div>
         @endif
     </div>
+    <div class="row1">
+        <label class="w33">~or~</label>
+        <div class="w67">
+              <div class="w67">
+                <div id="getTokenButton" class="button1Item">Get Token</div>
+                <div id="getTokenStatus" class="text3"></div>
+              </div>
+        </div>
+    </div>
+    <div class="row1">
+        <label class="w33"></label>
+        <div class="w67">
+              <div id="getTokenInstructions" class="w67 text2"></div>
+        </div>
+    </div>
+    <!-- <div class="row1">
+        <label class="w33">~or~</label>
+        <div class="w67">
+              <div class="w67">
+                <a class="a1" href="">Get token</a>
+              </div>
+        </div>
+    </div> -->
 
     <div class="row1{{ $errors->has('name') ? ' has-error' : '' }}">
         <label for="name" class="w33">Name</label>
@@ -28,19 +64,6 @@
         @if ($errors->has('name'))
           <div class="w33"></div>
           <div class="text2"> {{ $errors->first('name') }} </div>
-        @endif
-    </div>
-
-    <div class="row1{{ $errors->has('email') ? ' has-error' : '' }}">
-        <label for="email" class="w33">E-Mail Address</label>
-
-        <div class="w67">
-            <input id="email" type="email" class="w67" name="email" value="{{ $email }}" required>
-
-        </div>
-        @if ($errors->has('email'))
-          <div class="w33"></div>
-          <div class="text2"> {{ $errors->first('email') }} </div>
         @endif
     </div>
 
@@ -67,11 +90,20 @@
     <div class="row1">
       <div class="w33"></div>
       <div>
-          <button type="submit" class="btn btn-primary">
+          <button type="submit">
               Register
           </button>
       </div>
     </div>
 </form>
+
+@endsection
+
+@section('scripts')
+
+<!-- <script src="{{ asset('js/dragula/dragula.js') }}"></script> -->
+<script src="{{ asset('js/pivotlib.js') }}"></script>
+<!-- <script src="{{ asset('js/pivotWorkspace.js') }}"></script> -->
+<script src="{{ asset('js/register.js') }}"></script>
 
 @endsection
