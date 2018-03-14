62a4b4ffe5e98bec6a4eada55ff8122afcd96c00
tylerharter <tylerharter@gmail.com>
2018-03-14 18:42:01 -0500

    expose account deletion

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Mail;
+use App\Election;
+use App\EmailVerification;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Config;
+
+class AccountController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    public function delete_account(Request $request)
+    {
+        $user = Auth::user();
+        $user->delete();
+        // should probably redirect or something
+        return response()->json(array("result" => "deleted"));
+    }
+}
