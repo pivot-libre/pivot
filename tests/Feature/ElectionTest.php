a9c3a119c4c293c7a002938187f0ff419a8f1c3f
Oliver Nybroe <olivernybroe@gmail.com>
2018-04-05 11:01:31 +0200

    test: Cleanup in Election test

--- tests/Feature/ElectionTest.php
+++ tests/Feature/ElectionTest.php
@@ -15,13 +15,59 @@ use Tests\TestCase;
 
 class ElectionTest extends TestCase
 {
-    use DatabaseMigrations, DatabaseTransactions;
+    use DatabaseTransactions;
 
     /** @test */
     public function can_batch_vote()
     {
-        $this->disableExceptionHandling();
+        $user = factory(User::class)->create();
 
+        $election = factory(Election::class)->create();
+
+        /** @var Elector $elector */
+        $elector = factory(Elector::class)->create([
+            'user_id' => $user->id,
+            'election_id' => $election->id,
+        ]);
+
+        $candidateA = factory(Candidate::class)->create([
+            'name' => 'candidate-A',
+            'election_id' => $election->id,
+        ]);
+        $candidateB = factory(Candidate::class)->create([
+            'name' => 'candidate-B',
+            'election_id' => $election->id,
+        ]);
+        $candidateC = factory(Candidate::class)->create([
+            'name' => 'candidate-C',
+            'election_id' => $election->id,
+        ]);
+
+        $this->assertEquals(0, $elector->ranks()->count());
+        $response = $this->actingAs($user, 'api')->postJson("api/election/{$election->id}/batchvote", [
+            'votes' => [
+                [
+                    'candidate_id' => $candidateA->id,
+                    'rank' => 2
+                ],
+                [
+                    'candidate_id' => $candidateB->id,
+                    'rank' => 1
+                ],
+                [
+                    'candidate_id' => $candidateC->id,
+                    'rank' => 3
+                ]
+            ]
+        ]);
+
+        $response->assertStatus(200);
+        $this->assertEquals(3, $elector->ranks()->count());
+    }
+
+    /** @test */
+    public function can_batch_vote_when_()
+    {
         $user = factory(User::class)->create();
 
         $election = factory(Election::class)->create();
