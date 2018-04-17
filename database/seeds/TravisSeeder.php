4f5b01f08afa1d4697a3bfeb8aefb4275ca8371a
Oliver Nybroe <olivernybroe@gmail.com>
2018-04-17 10:41:06 +0200

    Test: Added Python to travis

--- database/seeds/TravisSeeder.php
+++ database/seeds/TravisSeeder.php
@@ -3,6 +3,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class TravisSeeder extends Seeder
 {
@@ -20,6 +21,22 @@ class TravisSeeder extends Seeder
             'email' => "travis-2@pivot.vote"
         ]);
 
+        $token1 = $user1->createToken('Token Name')->accessToken;
+        $token2 = $user2->createToken('Token Name')->accessToken;
 
+        $rootPath = base_path("tests/python");
+        $client = Storage::createLocalDriver(['root' => $rootPath]);
+        
+       $client->put('users.json', json_encode([
+            "users" => [
+                [
+                    'email' => $user1->email,
+                    'token' => $token1
+                ],[
+                    'email' => $user2->email,
+                    'token' => $token2
+                ],
+            ]
+        ]));
     }
 }
