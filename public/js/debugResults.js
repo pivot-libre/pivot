8d2bd4edaee751e75213a03e4b0e7cce6627dc69
tylerharter <tylerharter@gmail.com>
2018-06-23 10:36:26 -0500

    add candidate id=>name mapping

--- public/js/debugResults.js
+++ public/js/debugResults.js
@@ -23,29 +23,54 @@
     var PlotDiv = Piv.div(View.workspace, "Plot", "text1")
 
     function main() {
-	// generic setup
-	View.setHeader("Election Debugger", ElectionId)
-	View.statusbar.innerHTML = ""
-	Piv.electionsMenu(View.sidenav, ElectionId)
-	Piv.removeHrefsForCurrentLoc()
-	
-	// fetch snapshots
-	Piv.http.get(["/api/elections/" + ElectionId + "/result_snapshots"],
-		     getSnapshots, showErrorMessage)
+        // generic setup
+        View.setHeader("Election Debugger", ElectionId)
+        View.statusbar.innerHTML = ""
+        Piv.electionsMenu(View.sidenav, ElectionId)
+        Piv.removeHrefsForCurrentLoc()
+
+        // start workflow
+        getSnapshots()
     }
 
-    function getSnapshots(snapshots) {
-	snapshots.forEach(snapshot => {
-	    console.log(snapshot);
-	    piv.html(SnapshotsDiv, "span", snapshot.id + "<br>")
-	});
+    function getSnapshots() {
+        // fetch snapshots
+        Piv.http.get(["/api/elections/" + ElectionId + "/result_snapshots"],
+                     gotSnapshots, showErrorMessage)
+    }
+
+    function gotSnapshots(snapshots) {
+        snapshots.forEach(snapshot => {
+            var button = piv.html(SnapshotsDiv, "button", snapshot.id)
+            button.onclick = function() {
+                getSnapshot(snapshot.id)
+            }
+        })
+    }
+
+    function getSnapshot(SnapshotId) {
+        Piv.http.get(["/api/elections/" + ElectionId + "/result_snapshots/" + SnapshotId],
+                     gotSnapshot, showErrorMessage)
+    }
+
+    function gotSnapshot(snapshot) {
+        if (snapshot.format_version < 3) {
+            alert("You can only debug snapshots with version 3 or greater.  This one was version " + snapshot.format_version + ".")
+            return
+        }
+        console.log(snapshot)
+
+        CandidatesDiv.innerHTML = ""
+        snapshot.result_blob.debug_private.candidates.forEach(candidate => {
+            piv.html(CandidatesDiv, "span", candidate.id + ": " + candidate.name + "<br>")
+        })
     }
 
     function showErrorMessage(error) {
-	alert(error);
-	Piv.div(View.workspace, "", "w100 text3", "Results for this election are not currently available.")
-	if (!error) return
-	Piv.div(View.workspace, "", "100 text3", error.response.data.message)
+        alert(error)
+        Piv.div(View.workspace, "", "w100 text3", "Results for this election are not currently available.")
+        if (!error) return
+        Piv.div(View.workspace, "", "100 text3", error.response.data.message)
     }
 
     main(piv, election)
