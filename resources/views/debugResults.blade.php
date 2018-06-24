f8695dd23591ac43a2d1e58925ac9a925b44b942
tylerharter <tylerharter@gmail.com>
2018-06-24 18:40:18 -0500

    use alchemy to create dummy graphs

--- resources/views/debugResults.blade.php
+++ resources/views/debugResults.blade.php
@@ -1,9 +1,11 @@
-@extends('layouts.pivot')
+@extends('layouts.debug')
 
 @section('scripts')
 
 <script>var election = "{{$election}}"</script>
+<script src="{{ asset('js/alchemy/0.4.2/scripts/vendor.js') }}"></script>
+<script src="{{ asset('js/alchemy/0.4.2/alchemy.js') }}"></script>
 <script src="{{ asset('js/pivotlib.js') }}"></script>
 <script src="{{ asset('js/debugResults.js') }}"></script>
-
+    
 @endsection
