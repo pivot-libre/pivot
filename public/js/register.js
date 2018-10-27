119b4dca8f02977c0913454e1e4a968eb299555c
Carl Schroedl <carlschroedl@gmail.com>
2018-10-27 16:31:41 -0500

    Minor code quality fixes

--- public/js/register.js
+++ public/js/register.js
@@ -4,20 +4,20 @@
 (function(Piv) {
 
   //script-level variables
-  var Email = document.getElementById("email")
-  var GetTokenButton = document.getElementById("getTokenButton")
-  var GetTokenInstructions = document.getElementById("getTokenInstructions")
-  var RegisterStep1 = document.getElementById("registerStep1")
+  var Email = document.getElementById("email");
+  var GetTokenButton = document.getElementById("getTokenButton");
+  var GetTokenInstructions = document.getElementById("getTokenInstructions");
+  var RegisterStep1 = document.getElementById("registerStep1");
 
-  Piv.removeHrefsForCurrentLoc("removeContainer")
-  Piv.view.setHeader("Register")
+  Piv.removeHrefsForCurrentLoc("removeContainer");
+  Piv.view.setHeader("Register");
 
-  console.log("addEventListener")
+  console.log("addEventListener");
   GetTokenButton.addEventListener("click", function() {
-    console.log("getTokenButton")
-    GetTokenInstructions.innerHTML = "Sending Email..."
-    verifyEmail(Email.value, GetTokenInstructions)
-  })
+    console.log("getTokenButton");
+    GetTokenInstructions.innerHTML = "Sending Email...";
+    verifyEmail(Email.value, GetTokenInstructions);
+  });
 
   function getUrlParameter(name) {
     name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
@@ -26,8 +26,8 @@
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
   };
 
-  if (getUrlParameter("debug") != "1") {
-    if (getUrlParameter("token") != "") {
+  if (getUrlParameter("debug") !== "1") {
+    if (getUrlParameter("token") !== "") {
       RegisterStep1.style.display = "none";
     }
   }
@@ -35,9 +35,9 @@
   // function verifyEmail(email, statusEl, instructionsEl) {
   function verifyEmail(email, instructionsEl) {
     Piv.http.post(["/open/send_verify_email"], [{"email": email}], function(response) {
-      if ("confirmation email sent" == response) {
+      if ("confirmation email sent" === response) {
 
-        instructionsEl.innerHTML = "<p>Account registration started! </p><p>A confirmation email has been sent to " + email + ". Follow the link in the email to continue with your registration.</p>"
+        instructionsEl.innerHTML = "<p>Account registration started! </p><p>A confirmation email has been sent to " + email + ". Follow the link in the email to continue with your registration.</p>";
         var registrationEmailFields = document.getElementById("emailRegistrationGroup");
         registrationEmailFields.style.display = "none";
         var tokenRequestGroup = document.getElementById("tokenRequestGroup");
@@ -50,4 +50,4 @@
   }
 
   // close the self-executing function and feed the piv library to it
-})(piv)
+}(piv));
