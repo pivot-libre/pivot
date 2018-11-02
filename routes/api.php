246132c27f91aa9a508b06bedb91fdea328bd4c4
Carl Schroedl <carlschroedl@gmail.com>
2018-11-02 18:15:14 -0500

    First cut of a stateless BFF election-runner

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('calculateResult', 'StatelessElectionController@calculateResult')->name('calculateResult');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
