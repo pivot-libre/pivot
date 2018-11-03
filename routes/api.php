17d201f68a76568a26b86c5c8a696b966d494287
Carl Schroedl <carlschroedl@gmail.com>
2018-11-03 16:23:55 -0500

    Moved stateless election route to unauthenticated open route.

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('calculateResult', 'StatelessElectionController@calculateResult')->name('calculateResult');
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
--- routes/open.php
+++ routes/open.php
@@ -2,4 +2,5 @@
 
 use Illuminate\Http\Request;
 
+Route::post('calculateResult', 'StatelessElectionController@calculateResult')->name('calculateResult');
 Route::post('/send_verify_email', 'VerifyController@send_verify_email');
