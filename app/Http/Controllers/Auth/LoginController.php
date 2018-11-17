4556fa9bc63591390ec48643c8e7a8a2937947f8
Carl Schroedl <carlschroedl@gmail.com>
2018-11-17 16:08:43 -0600

    Updated redirect on login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-     protected $redirectTo = '/';
+     protected $redirectTo = '/myElections';
     // protected $redirectTo = '/profile';
 
     /**
